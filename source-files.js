de27ec933f63b935d2ec964c1bd909b261334f04
umcconnell <umcconnell@users.noreply.github.com>
2021-01-10 15:44:43 +0000

    deploy: d65a06c7279af128d9af2fa033a84aafea9bb858

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
+sourcesIndex["cargo_servum"] = {"name":"","files":["main.rs"]};
 sourcesIndex["servum"] = {"name":"","dirs":[{"name":"cli","files":["config.rs","err.rs","tui.rs"]},{"name":"files","files":["file.rs","mime.rs","path.rs"]},{"name":"http","files":["handler.rs","html.rs","request.rs","request_err.rs","response.rs","status.rs"]},{"name":"multiprocessing","files":["message.rs","threadpool.rs","worker.rs"]}],"files":["cli.rs","files.rs","http.rs","lib.rs","multiprocessing.rs"]};
 createSourceSidebar();
