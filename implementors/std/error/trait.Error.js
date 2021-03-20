055f1958063b5a0a0d24aeee6305057f5e33ffbd
umcconnell <umcconnell@users.noreply.github.com>
2021-03-20 12:29:33 +0000

    deploy: d60b606499d36aecee8cd4c821031c89ba8e2e7e

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["servum"] = [{"text":"impl&lt;'_&gt; Error for CliError&lt;'_&gt;","synthetic":false,"types":[]},{"text":"impl Error for HTTPRequestError","synthetic":false,"types":[]}];
+implementors["servum"] = [{"text":"impl Error for CliError&lt;'_&gt;","synthetic":false,"types":[]},{"text":"impl Error for HTTPRequestError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
